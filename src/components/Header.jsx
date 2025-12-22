009d0e1979bbef9fcb69c2d81f84b4f6918c6826
Pritom099 <pritompaul515@gmail.com>
2025-12-22 20:08:35 +0600

    third commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from "../assets/logo.png"
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
-        <div className='flex justify-center items-center flex-col'>
-            <img className='w-[350px]' src={logo} alt="" />
+        <div className='flex justify-center items-center flex-col gap-2'>
+            <img className='w-[400px]' src={logo} alt="" />
             <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='text-accent font-semibold'>{format(new Date(), "EEEE, MMMM MM, yyyy")}</p>
         </div>
     );
 };
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import Header from '../components/Header';
 import { Outlet } from 'react-router';
+import LatestNews from '../components/LatestNews';
+import Navbar from '../components/Navbar';
+import LeftAside from '../components/homelayout/LeftAside';
+import RightAside from '../components/homelayout/RightAside';
 
 const HomeLayout = () => {
     return (
-        <div>
+        <div className='my-8'>
             <header>
                 <Header></Header>
             </header>
-            <main>
-                <section className='left-nav'></section>
-                <section className='main'>
+            <section className='w-11/12 mx-auto my-5'>
+                <LatestNews></LatestNews>
+            </section>
+            <nav className='w-11/12 mx-auto my-5'>
+                <Navbar></Navbar>
+            </nav>
+            <main className='w-11/12 mx-auto my-5  grid grid-cols-12'>
+                <aside className='col-span-3'>
+                    <LeftAside></LeftAside>
+                </aside>
+                <section className='main col-span-6'>
                     <Outlet></Outlet>
                 </section>
-                <section className='right-nav'></section>
+                <aside  className='col-span-3'>
+                    <RightAside></RightAside>
+                </aside>
             </main>
         </div>
     );
