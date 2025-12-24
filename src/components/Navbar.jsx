b331d8aa3274da6c3a43da4c97e6360e1cb0491c
Pritom099 <pritompaul515@gmail.com>
2025-12-24 16:56:33 +0600

    seventh commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,18 @@ import userIcon from "../assets/user.png"
 import { AuthContext } from '../Provider/AuthProvider';
 
 const Navbar = () => {
-    const {user} = use(AuthContext);
+    const { user, logOut } = use(AuthContext);
+
+    const handleLogOut =() => {
+        logOut()
+        .then(() => {
+            alert("You logged out successfullly");
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
     return (
         <div className='flex justify-between items-center'>
             <div className=''>{user && user.email}</div>
@@ -16,9 +27,15 @@ const Navbar = () => {
             </div>
             <div className='login-btn flex gap-5'>
                 <img src={userIcon} alt="" />
-                <Link to="/auth/login" className='btn btn-primary px-10'>Login</Link>
-            </div>
-        </div>
+                {
+                    user ? (
+                        <button onClick={handleLogOut} className = 'btn btn-primary px-10'>LogOut</button>
+                    ): 
+                    (<Link to = "/auth/login" className = 'btn btn-primary px-10'>Login</Link>)
+                }
+
+             </div>
+        </div >
     );
 };
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,30 +1,52 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const Register = () => {
+    const {createUser, setUser} = use(AuthContext);
+    const handleRegister = (e) => {
+        e.preventDefault();
+        //console.log(e.target)
+        const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log({name, photo, email, password});
+        createUser(email, password)
+        .then(result => {
+            const user = result.user;
+            // console.log(user);
+            setUser(user);
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            alert(errorMessage);
+        });
+    }
     return (
         <div className='flex justify-center items-center min-h-screen'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
                 <h2 className='font-semibold text-2xl text-center'>Register your account</h2>
-                <div className="card-body">
+                <form onSubmit={handleRegister} className="card-body">
                     <fieldset className="fieldset">
                         {/* name */}
                         <label className="label">Name</label>
-                        <input type="text" className="input" placeholder="Enter your name" />
+                        <input type="text" name='name' className="input" placeholder="Enter your name" required />
                         {/* photo */}
                         <label className="label">Photo URL</label>
-                        <input type="text" className="input" placeholder="Photo URL" />
+                        <input type="text" name='photo' className="input" placeholder="Photo URL" required />
                         {/* email */}
                         <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Enter your Email" />
+                        <input type="email" name='email' className="input" placeholder="Enter your Email" required />
                         {/* password */}
                         <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Enter your Password" />
+                        <input type="password" name='password' className="input" placeholder="Enter your Password" required />
                     
-                        <button className="btn btn-neutral mt-4">Register</button>
+                        <button type='submit' className="btn btn-neutral mt-4">Register</button>
                         <p className='text-center font-semibold pt-5'>Already Have An Account ? <Link className='text-secondary' to="/auth/login">Login</Link></p>
                     </fieldset>
-                </div>
+                </form>
             </div>
         </div>
 
